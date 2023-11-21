8717d4a0b72dc6e97ab15f4e461380374d3aa620
Edwin-1234 <146200949+Edwin-1234@users.noreply.github.com>
2023-11-21 21:24:22 +0300

    Create sumofintegers.java

---
+++ string tokenizer(util)/sumofintegers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class sumofintegers
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader rde=new BufferedReader(new InputStreamReader(System.in));
+        System .out.println("ENter a line of integers");
+        String num=rde.readLine();
+        StringTokenizer t=new StringTokenizer(num);
+        int sum=0;
+        while(t.hasMoreTokens())
+        {
+            String n=t.nextToken();
+            int nub=Integer.parseInt(n);
+            sum=sum+nub;
+        }
+        System.out.println("Sum="+sum);    
+    }
+}
