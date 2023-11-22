419ce63be7874c58d940b2db110c0b50f0b5a6ee
Edwin Varkey <146200949+Edwin-1234@users.noreply.github.com>
2023-11-23 02:59:31 +0300

    Create gui1.java

---
+++ GUI/gui1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class gui extends JFrame
+{
+    public static void main(String[] args) 
+    {
+        JFrame f=new JFrame("Window");
+        f.setBounds(100,100,300,400);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);    
+    }
+}
