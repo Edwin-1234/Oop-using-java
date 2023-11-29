549a2d8709733b101f728defea25447c7cfd6255
Edwin Varkey <146200949+Edwin-1234@users.noreply.github.com>
2023-11-29 14:50:58 +0530

    Update threadl2.java

--- threads/threadl2.java
+++ threads/threadl2.java
@@ -1,84 +1,78 @@
-import java.util.Random;
-class flagvariables
+import java.util.*;
+import java.lang.*;
+class flagvari
 {
-    static boolean enumgen,onumgen;
-    static int num;
+   static int num;
+   static boolean enumgen;
+   static boolean onumgen;
 }
 class rand extends Thread
 {
-    public void run()
-    {
-        Random r=new Random();
-        while(true)
-        {
-            while(flagvariables.enumgen||flagvariables.onumgen)
-            {
-                
-            }
-            int n=r.nextInt();
-            flagvariables.num=n;
-            if(n%2==0)
-            {
-                flagvariables.enumgen=true;
-            }
-            else
-            {
-                flagvariables.onumgen=true;
-            }
-        }
+   public void run()
+   {
+      Random r=new Random();
+      while(true)
+      {
+         while(flagvari.enumgen||flagvari.onumgen);
+             int n=r.nextInt(10)+1;
+             flagvari.num=n;
+             if(n%2==0)
+             {
+                flagvari.enumgen=true;
+             }
+             else
+             {
+                flagvari.onumgen=true;
+             }
+      }
     }
 }
 class Square extends Thread
 {
-    public void run()
-    {
-        while(true)
-        {
-            while(!flagvariables.enumgen)
-            {
-                
-            }
-            System.out.println("Square="+flagvariables.num*flagvariables.num);
-            flagvariables.enumgen=false;
-        }
+   public void run()
+   {
+      while(true)
+      {
+        while(!flagvari.enumgen);
+          System.out.println(flagvari.num+ ":Square="+flagvari.num*flagvari.num);
+          flagvari.enumgen=false;
+      }
     }
 }
 class Cube extends Thread
 {
-    public void run()
-    {
-        while(true)
-        {
-            while(!flagvariables.onumgen)
-            {
-                
-            }
-            System.out.println("Cube="+flagvariables.num*flagvariables.num*flagvariables.num);
-            flagvariables.onumgen=false;
-        }
-    }
+   public void run()
+   {
+     while(true)
+     {
+        while(!flagvari.onumgen);
+           System.out.println(flagvari.num+ ":Cube="+flagvari.num*flagvari.num*flagvari.num);
+           flagvari.onumgen=false;
+     }
+   }
 }
-public class threadlab
+class pgm11
 {
-    public static void main(String[] args)
-    {
-        rand r=new rand();
-        Square s=new Square();
-        Cube c=new Cube();
-        flagvariables.onumgen = false;
-        flagvariables.enumgen = false;
-        r.start();
-        s.start();
-        c.start();
-        try 
-        {
-            r.join();
-            s.join();
-            c.join();
-        }
-        catch(Exception e)
-        {
-            System.out.println("Error"+e);
-        }
-    }
+   public static void main(String[] args)
+   {
+     rand r=new rand();
+     Square s=new Square();
+     Cube c=new Cube();
+     flagvari.enumgen=false;
+     flagvari.onumgen=false;
+     r.start();
+     s.start();
+     c.start();
+     try
+     {
+         r.join();
+         s.join();
+         c.join();
+     }
+     catch(Exception e)
+     {
+         System.out.println(e);
+     }
+   }
 }
+
