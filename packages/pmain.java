2825a66467d9ea91f68ca2f6c9968e5af76cb6ed
Edwin Varkey <146200949+Edwin-1234@users.noreply.github.com>
2023-12-06 13:22:50 +0300

    Create pmain.java

---
+++ packages/pmain.java
@@ -0,0 +1,12 @@
+import packageone.a;
+import packagetwo.b;
+public class pmain
+{
+    public static void main(String[] args)
+    {
+        a a1=new a();
+        a1.method1();
+        b b1=new b();
+        b1.method2();
+    }
+}
